c1b5940b54c4d5c5995603a2c22a0abffcdd65e2
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2023-10-15 17:08:51 -0300

    feat: deleted unnecessary code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,35 +1,4 @@
-import '@/styles/global.css';
-
-import type { Metadata } from 'next';
-
-export const metadata: Metadata = {
-  icons: [
-    {
-      rel: 'apple-touch-icon',
-      url: '/apple-touch-icon.png',
-    },
-    {
-      rel: 'icon',
-      type: 'image/png',
-      sizes: '32x32',
-      url: '/favicon-32x32.png',
-    },
-    {
-      rel: 'icon',
-      type: 'image/png',
-      sizes: '16x16',
-      url: '/favicon-16x16.png',
-    },
-    {
-      rel: 'icon',
-      url: '/favicon.ico',
-    },
-  ],
-};
-
 export default function RootLayout({
-  // Layouts must accept a children prop.
-  // This will be populated with nested layouts or pages
   children,
 }: {
   children: React.ReactNode;
@@ -40,8 +9,3 @@ export default function RootLayout({
     </html>
   );
 }
-
-// Enable edge runtime but you are required to disable the `migrate` function in `src/libs/DB.ts`
-// Unfortunately, this also means it will also disable the automatic migration of the database
-// And, you will have to manually migrate it with `npm run db:push`
-// export const runtime = 'edge';
