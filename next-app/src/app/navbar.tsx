e7887b633ec52748ff32945e3b9fe343f80465af
Luis Paredes <lparedesr@fi.uba.ar>
2023-11-02 16:54:03 -0300

    Added log out button

--- next-app/src/app/navbar.tsx
+++ next-app/src/app/navbar.tsx
@@ -6,7 +6,7 @@ import Image from 'next/image';
 import { usePathname } from 'next/navigation';
 import type { DefaultSession } from 'next-auth';
 import { signIn, signOut } from 'next-auth/react';
-import { Fragment } from 'react';
+import { Fragment, useEffect, useState } from 'react';
 
 import logOut from '@/firebase/auth/signOut';
 
@@ -43,27 +43,7 @@ export default function Navbar({ user }: { user: DefaultSession['user'] }) {
             <div className="flex h-16 justify-between">
               <div className="flex">
                 <div className="flex shrink-0 items-center">
-                  {/* <svg
-                    width="32"
-                    height="32"
-                    viewBox="0 0 32 32"
-                    fill="none"
-                    className="text-gray-100"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <rect
-                      width="100%"
-                      height="100%"
-                      rx="16"
-                      fill="currentColor"
-                    />
-                    <path
-                      fillRule="evenodd"
-                      clipRule="evenodd"
-                      d="M17.6482 10.1305L15.8785 7.02583L7.02979 22.5499H10.5278L17.6482 10.1305ZM19.8798 14.0457L18.11 17.1983L19.394 19.4511H16.8453L15.1056 22.5499H24.7272L19.8798 14.0457Z"
-                      fill="black"
-                    />
-                  </svg> */}
+                  
                   <Image
                         className="h-18 w-18 rounded-full"
                         src={user?.image || 'https://avatars.githubusercontent.com/t/8529037?s=116&v=4'}
@@ -91,6 +71,7 @@ export default function Navbar({ user }: { user: DefaultSession['user'] }) {
                 </div>
               </div>
               <div className="hidden sm:ml-6 sm:flex sm:items-center">
+                
                 <Menu as="div" className="relative ml-3">
                   <div>
                     <Menu.Button className="flex rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-slate-500 focus:ring-offset-2">
@@ -157,11 +138,12 @@ export default function Navbar({ user }: { user: DefaultSession['user'] }) {
                             </button>
                           )}
                         </Menu.Item>
+
                     </Menu.Items>
                   </Transition>
                 </Menu>
               </div>
-              <div className="-mr-2 flex items-center sm:hidden">
+              {/* <div className="-mr-2 flex items-center sm:hidden">
                 <Disclosure.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-slate-500 focus:ring-offset-2">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
@@ -170,10 +152,10 @@ export default function Navbar({ user }: { user: DefaultSession['user'] }) {
                     <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
                   )}
                 </Disclosure.Button>
-              </div>
+              </div> */}
             </div>
           </div>
-
+{/* 
           <Disclosure.Panel className="sm:hidden">
             <div className="space-y-1 pb-3 pt-2">
               {navigation.map((item) => (
@@ -237,7 +219,7 @@ export default function Navbar({ user }: { user: DefaultSession['user'] }) {
                 </div>
               )}
             </div>
-          </Disclosure.Panel>
+          </Disclosure.Panel> */}
         </>
       )}
     </Disclosure>
