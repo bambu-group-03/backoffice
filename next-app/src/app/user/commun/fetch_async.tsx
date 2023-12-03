e9ea5cc6e4b5c594fb50769c8d800f888efa74e9
Luis Paredes <lparedesr@fi.uba.ar>
2023-12-03 12:45:02 -0300

    Added def var

--- next-app/src/app/user/commun/fetch_async.tsx
+++ next-app/src/app/user/commun/fetch_async.tsx
@@ -1,24 +1,27 @@
-// import { Env } from '@env';
-// import axios from 'axios';
-// // Configurar encabezados por defecto para todas las solicitudes
-// // Instancia para el microservicio de contenido
-// const contentService = axios.create({ baseURL: `${Env.API_URL}/content` });
+import axios from 'axios';
 
-// // Instancia para el microservicio de identidad
-// const identityService = axios.create({ baseURL: `${Env.API_URL}/identity` });
+// Configurar encabezados por defecto para todas las solicitudes
+// Instancia para el microservicio de contenido
+const contentService = axios.create({ baseURL: `${process.env.API_URL}/content` });
 
-// contentService.defaults.headers.common.clientToken = 'ADMIN_TOKEN';
-// contentService.defaults.headers.common['Content-Type'] = 'application/json';
-// contentService.defaults.headers.common.Accept = 'application/json';
+// Instancia para el microservicio de identidad
+const identityService = axios.create({ baseURL: `${process.env.API_URL}/identity` });
 
-// identityService.defaults.headers.common.clientToken = 'ADMIN_TOKEN';
-// identityService.defaults.headers.common['Content-Type'] = 'application/json';
-// identityService.defaults.headers.common.Accept = 'application/json';
 
-// export const client = {
-//   content: contentService,
-//   identity: identityService,
-// };
+contentService.defaults.headers.common.adminToken =  `${process.env.ADMIN_TOKEN}`;
+contentService.defaults.headers.common['Content-Type'] = 'application/json';
+contentService.defaults.headers.common.Accept = 'application/json';
+
+identityService.defaults.headers.common.adminToken = `${process.env.ADMIN_TOKEN}`;
+identityService.defaults.headers.common['Content-Type'] = 'application/json';
+identityService.defaults.headers.common.Accept = 'application/json';
+
+
+
+export const client = {
+  content: contentService,
+  identity: identityService,
+};
 
 const REFRESH_INTERVAL = 1000 * 60 * 60 * 24; // 24 hours
 
