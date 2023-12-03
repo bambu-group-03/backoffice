e0ed675d21b8596cb0e6d175a9e313aff7e74350
Luis Paredes <lparedesr@fi.uba.ar>
2023-12-02 21:56:08 -0300

    Small refactor

--- next-app/src/app/admin/page.tsx
+++ next-app/src/app/admin/page.tsx
@@ -75,11 +75,11 @@ export default async function AdminPage(){
             </div>
           </div>
         </div>
-        <h1>Other Admins</h1>
-      <AdminTable admins={admins}/>
-      </div>
 
-     
+        <h1>Other Admins</h1>
+          <AdminTable admins={admins}/>
+          
+      </div>
     </main>
   );
 }
